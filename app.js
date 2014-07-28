ca4afb9d7dde8b53e2c0dbf7e7b58abc1d1572ac
Hoolean <me@hoolean.com>
2014-07-28 15:15:57 +0100

    Updated Express to Express 4 and fixed some inconsistencies)

--- app.js
+++ app.js
@@ -3,38 +3,24 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes');
-var app = module.exports = express.createServer();
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+var routes = require('./routes');
+
+var app = module.exports = express();
 
 // Configuration
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
-
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
-
-
-
-
-
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(bodyParser());
+app.use(methodOverride());
+app.use(express.static(__dirname + '/public'));
 
 // Routes
 
 app.get('/', routes.index);
 
-app.listen(3000, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
-});
+app.listen(3000);
