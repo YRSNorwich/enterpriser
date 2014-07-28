d504619c557cc31b82991be5dffd8c18868b9227
oliver <oliver@neontribe.co.uk>
2014-07-28 16:29:13 +0100

    Added yahoo data parsing

--- app.js
+++ app.js
@@ -3,9 +3,28 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes');
+var express = require('express'),
+    routes = require('./routes'),
+    yahoo = require('./yahoo.js'),
+    Parser = require('babyparse'),
+    fs = require('fs');
+
 var app = module.exports = express.createServer();
+
+var config = {
+  delimiter: "",
+  header: false,
+  dynamicTyping: false,
+  preview: 0,
+  step: undefined,
+  encoding: "",
+  worker: false,
+  comments: false,
+  complete: undefined,
+  download: false,
+  keepEmptyRows: false,
+  chunk: undefined
+}
 
 // Configuration
 
@@ -26,6 +45,37 @@ app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
+
+test();
+
+
+
+function test() {
+  
+  var Yahoo = new yahoo();
+  
+  var googleQuery = Yahoo.buildQuery("GOOGL", "10/04/2014");
+
+
+  var parser = new Parser( config );
+
+  
+
+  googleQuery.request(function(data) {
+  
+  // Convert CSV to JSON
+  var results = JSON.stringify(parser.parse(data).results);
+
+  fs.writeFile(googleQuery.stock.substr(3), results, function(err) {
+    console.log("There was AN ERROR!" + err);
+  });
+
+
+
+});
+
+
+}
 
 
 
--- yahoo.js
+++ yahoo.js
@@ -1,39 +1,61 @@
+var http = require("http");
+
+
 (function() {
+	var key = '&oauth=6e874e71d6bf3b10888b7d20615a9f3e95dce5ab';
+	var yahoo = "http://ichart.finance.yahoo.com/table.csv?";
+	var debug = true;
+function Yahoo() {
 
-var key = '&oauth=6e874e71d6bf3b10888b7d20615a9f3e95dce5ab';
-var yahoo = "http://ichart.finance.yahoo.com/table.csv";
-var debug = true;
-
+}
 
 /* Contructors */
-var Query = function(stock, data) {
-	this.url = url;
-	this.data = data;
+function Query(stock, to) {
+
 	this.stock = "&s=" + stock;
-		this.to = "&c=" + data[0];
-		this.from = data[1];
-}
+	this.to = "&c=" + to;
 
-Query.prototype.build = function() {
-	//Build an api query
-	var temp = this.url + this.data + this.to + this.from + key;
-	return temp;
-}
+	this.url = "";
+	
+	this.build = function() {
+		//Build an api query
+		var temp = yahoo + this.stock + this.to + key;
+		this.url = temp;
+		return temp;
+	};
+	
+	this.request = function(callback) {
+	
+	// Utility function that downloads a URL and invokes
+	// callback with the data.
+	  http.get(this.url, function(res) {
+	    var data = "";
+	    res.on('data', function (chunk) {
+	      data += chunk;
+	    });
+	    res.on("end", function() {
+	      callback(data);
+	    });
+	  }).on("error", function() {
+	    callback(null);
+	  });
+	}
 
-Query.protoype.request = function() {
 
-}
+
+};
 
 /* Methods */
 
 
-function buildQuery(stock, to, from) {
-	var query = new Query(yahoo, [to, from]).build();
+Yahoo.prototype.buildQuery = function(stock, to, from) {
+	var query = new Query(stock, to);
+	console.log(query.build());
 	if(debug) console.log(query);
 	return query;
 }
 
-
+module.exports = Yahoo;
 
 
 })();
