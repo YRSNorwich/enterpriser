2a6ff844a72a71536b23b597fe539efec6f6ba93
oliver <oliver@neontribe.co.uk>
2014-07-29 20:40:08 +0100

    Autocomplete should be done, awaiting testing

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -9,16 +9,27 @@
       var companyFinderAutocomplete = $("#companySearch");
 
       //Autocomplete
-
+      //Shizzy jQuery ui autocomplete
       companyFinderAutocomplete.autocomplete({
-        source: "data",
+        source: function(req, res) {
+          $.getJSON("http://127.0.0.1:3000/ajax/list/", function(data) {
+            res(data);
+          })
+        },
+        select: function(event, ui) {
+          companyFinderAutocomplete.val(ui.item.title);
+          return false;
+        },
         messages: {
           noResults: '',
           results: function() {}
         },
         focus: function (event, ui) {
-            $(".ui-helper-hidden-accessible").hide();
+              //Set content to value just for sake of niceness
+              companyFinderAutocomplete.val(ui.item.title);
+              $(".ui-helper-hidden-accessible").hide();
               event.preventDefault();
+              return false;
         }
 
       })
@@ -28,5 +39,5 @@
 
 
 
-    
+
 })(jQuery)
