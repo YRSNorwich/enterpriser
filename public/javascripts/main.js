f8a5cfafcc3c46ecf8bb1a79bf936801abc14543
oliver <oliver@neontribe.co.uk>
2014-07-29 21:20:12 +0100

    AUTOCOMPLETE WORKSnpm start! HOLY SHITTTnpm startnpm start

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -10,23 +10,36 @@
 
       //Autocomplete
       //Shizzy jQuery ui autocomplete
+
+
+
       companyFinderAutocomplete.autocomplete({
+        minLength: 1,
         source: function(req, res) {
+
+
           $.getJSON("http://127.0.0.1:3000/ajax/list/", function(data) {
-            res(data);
-          })
+            var newList = [];
+            for (var i in data) {
+              if( data[i].search(req.term) !== -1 ) {
+                newList.push(data[i]);
+              } else {
+
+              }
+            }
+            console.log(newList);
+            res(newList);
+
+          });
         },
-        select: function(event, ui) {
-          companyFinderAutocomplete.val(ui.item.title);
-          return false;
-        },
+        select: function(event, ui) { },
         messages: {
           noResults: '',
           results: function() {}
         },
         focus: function (event, ui) {
               //Set content to value just for sake of niceness
-              companyFinderAutocomplete.val(ui.item.title);
+              companyFinderAutocomplete.val(ui.item.label);
               $(".ui-helper-hidden-accessible").hide();
               event.preventDefault();
               return false;
