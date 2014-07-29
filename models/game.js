9dddfc7f9099dc763fe09af756f67bf61295a963
Hoolean <me@hoolean.com>
2014-07-29 14:35:21 +0100

    Added Game schema

--- models/game.js
+++ models/game.js
@@ -1,8 +1,19 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+/*
+ * Bought is a dictionary in the form:
+ * {
+ * 	Stock ID: Number
+ * }
+ */
 var gameSchema = new Schema({
-	
+	companyName: String,
+	companyId: String,
+	balance: { type: Number, default: 10000 },
+	shares: { type: Number, default: 1000 },
+	bought: { type: {}, default: {} },
+	day: { type: Date, default: '1/1/2004' }
 });
 
 module.exports = exports = mongoose.model('Game', gameSchema);
