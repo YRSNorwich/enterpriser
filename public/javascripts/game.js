35a6531da7b30634ba8d1707ee00c3abf8a03c34
Hoolean <me@hoolean.com>
2014-07-31 16:20:00 +0100

    Made left side of playview look pwetty

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -68,11 +68,11 @@ Game.prototype.setPortfolio = function(data, element, callback) {
 }
 
 jQuery("body").on("click", ".portfolioSelect", function(e){
-  var id = jQuery(this).attr("href"));
-//  game.setPortData(this.gameData["bought"], id)
-
 
   e.preventDefault();
+  var id = jQuery(this).attr("href");
+  //game.setPortData(this.gameData["bought"], id)
+
 }.bind(this));
 
 Game.prototype.setPortData = function(data1, i) {
